dd1877bdf89d2c6ae0425cf8df4d43ef97203575
JulienP58 <paillardjulien58@gmail.com>
2024-06-10 18:24:37 +0200

    [ADD] moving spaceship

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var is_in_game = false;
 var body = document.body;
 
+var ship = document.createElement("img");
+var positionX = window.innerWidth / 2;
+
+ship.src = "assets/ship.png";
+ship.style.scale = 0.1;
+
+ship.style.position = "fixed";
+ship.style.bottom = "0";
+ship.style.left = positionX + "px";
+ship.style.transform = "translateX(-50%)";
+
 function linkButton() {
     var button_play = document.querySelectorAll('.button_play')[0];
     var button_settings = document.querySelectorAll('.button_settings')[0];
@@ -8,6 +19,7 @@ function linkButton() {
     button_play.addEventListener("click", function () {
         button_play.remove();
         button_settings.remove();
+        body.appendChild(ship);
         is_in_game = true;
     });
 
@@ -18,6 +30,7 @@ function linkButton() {
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape" && is_in_game == true) {
+        ship.remove();
         is_in_game = false;
         var buttonsDiv = document.createElement("div");
         buttonsDiv.classList.add("buttons");
@@ -33,6 +46,14 @@ document.addEventListener("keydown", function(event) {
         body.appendChild(buttonsDiv);
 
         linkButton();
+    }
+    if (event.key == "ArrowLeft" && is_in_game == true) {
+        positionX -= 30;
+        ship.style.left = positionX + "px";
+    }
+    if (event.key == "ArrowRight" && is_in_game == true) {
+        positionX += 30;
+        ship.style.left = positionX + "px";
     }
 });
 
